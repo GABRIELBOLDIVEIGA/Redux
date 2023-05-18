9639eac1195c45c3358fc8ddf0e2a66961282069
Gabriel Boldi <gabriel.boldi@protonmail.com>
2023-05-18 20:22:03 -0300

    ...

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import PaginaPadrao from 'components/PaginaPadrao';
 import Home from 'pages/Home';
+import Categoria from 'pages/Categoria';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function Router() {
@@ -8,8 +9,8 @@ export default function Router() {
       <Routes>
         <Route path='/' element={<PaginaPadrao />}>
           <Route index element={<Home />} />
+          <Route path='/categoria/:nomeCategoria' element={<Categoria />} />
         </Route>
-
         <Route path='*' element={<h1>Algo de Errado não esta certo...</h1>}/>
       </Routes>
     </BrowserRouter>
